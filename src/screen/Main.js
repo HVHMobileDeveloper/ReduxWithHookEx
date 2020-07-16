df9d766740b6b98c4972563cf9bfead532a20276
HiepHuynh <hiep.vh@techatrium.com>
2020-07-16 16:40:30 +0700

    [Hook] UseState, UseEffect, UseMemo, UseCallBack

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -6,30 +6,54 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
-import { View , Text, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { inCrease, deCrease } from '../redux';
 
 const Main = () => {
 
 
+    //TODO:REDUX: USE SELECTOR, USE DISPACH
     const value = useSelector(state => state)
-  const dispatch = useDispatch()
-  const increase = () => dispatch(inCrease())
-  const decrease = () => dispatch(deCrease())
+    const dispatch = useDispatch()
+    const increase = () =>{ dispatch(inCrease()), setLoading(!isLoading)}
+    const decrease = () => { dispatch(deCrease()), setLoading(!isLoading)}
+
+    //USE STATE
+    const [isLoading, setLoading] = useState(false);
+
+    //USE EFFECT
+    useEffect(()=>{
+        setLoading(true)
+    })
+
+    //USE_MEMO
+    const checkLoading = useMemo(() => <Text>{`${isLoading} ${value}`}</Text>, [isLoading]);
+
+
+    //User Callback
+    const handleIncrement = useCallback( increase, [value]);
+    const handleDeCrement = useCallback( decrease, [value])
 
 
     return (
-        <View style={{flex: 1, justifyContent:'center'}}>
-            <TouchableOpacity onPress={increase}>
-                <Text>InCrease</Text>
-            </TouchableOpacity>
-            <Text>{value}</Text>
-            <TouchableOpacity onPress={decrease}>
-                <Text>DeCrease</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button title="increment" onPress={handleIncrement} />
+            {checkLoading}
+            <Button title="decrement" onPress={handleDeCrement} />
         </View>
+    )
+}
+
+export const Button = ({title, onPress}) => {
+    useEffect(() =>{
+        console.log("btn click")
+    })
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <Text>{title}</Text>
+        </TouchableOpacity>
     )
 }
 
