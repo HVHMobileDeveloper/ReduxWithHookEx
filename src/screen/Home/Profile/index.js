9b72092f0b544ec0477f5bae958bb624ce63df89
HiepHuynh <hiep.vh@techatrium.com>
2020-07-18 23:06:04 +0700

    [Information] display info to view

--- src/screen/Home/Profile/index.js
+++ src/screen/Home/Profile/index.js
@@ -5,11 +5,20 @@ import { useSelector } from 'react-redux';
 
 const Profile = ({ navigation }) => {
 
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+
     const value = useSelector(state => state.ReducerProfile)
+    useEffect(()=>{
+        const { email, password} = value
+        setEmail(email)
+        setPass(password)
+    })
 
     return (
-        <View style={{ backgroundColor: 'yellow', flex: 1, }}>
-            <Text>{`${JSON.stringify(value)}`}</Text>
+        <View style={{ backgroundColor: 'yellow', flex: 1, flexDirection: 'row'}}>
+            <Text>{`Email: ${email}`}</Text>
+            <Text>{`Password: ${pass}`}</Text>
         </View>
     )
 }
